d44525d603aedc8cd4ddcf1de5326d5546f2840c
arrowgtp <contact@arrowgtp.com>
2019-06-06 10:50:21 -0700

    soooo frustrated

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -35,9 +35,9 @@
 //   return <Div100vh>{element}</Div100vh>
 // }
 
-import React from 'react'
-import Div100vh from 'react-div-100vh'
+// import React from 'react'
+// import Div100vh from 'react-div-100vh'
 
-export const wrapPageElement = ({ element }) => {
-  return <Div100vh>{element}</Div100vh>
-}
+// export const wrapPageElement = ({ element }) => {
+//   return <Div100vh>{element}</Div100vh>
+// }
