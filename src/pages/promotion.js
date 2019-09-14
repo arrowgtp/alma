298e90b56f8978cd21db793e8c496354c6aacfb2
Arrow GTP <contact@arrowgtp.com>
2019-09-13 18:33:25 -0700

    ugh so many times...

--- src/pages/promotion.js
+++ src/pages/promotion.js
@@ -140,10 +140,10 @@ const Promotion = () => {
         name="contact-form"
         method="post"
         data-netlify="true"
-        // data-netlify-honeypot="bot-field"
+        data-netlify-honeypot="bot-field"
       >
-        {/* <input type="hidden" name="bot-field" /> */}
         <input type="hidden" name="contact-form" value="contact"/>
+        <input type="hidden" name="bot-field" />
         <Message>
           Don’t miss out on a chance to win your next exotic getaway! Enter now!
         </Message>
